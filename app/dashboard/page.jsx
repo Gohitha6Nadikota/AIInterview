d6f654ad023f1764ff45ff705f8ca6810bfb532f
GohithaNadikota <gohithapriyanadikota@gmail.com>
2024-10-28 23:09:39 +0530

    Name fix

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
-import AddInterview from "./_components/addInterview";
+import AddInterview from "./_components/AddInterview";
 import Interviews from "./_components/Interviews";
 function DashBoard() {
   return (
